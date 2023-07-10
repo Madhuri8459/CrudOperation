23d3c002966a8f54649fc1620437b8a59a36f4ac
Madhuri8459 <131444689+Madhuri8459@users.noreply.github.com>
2023-07-10 14:57:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+import CRUD from'./components/CRUD';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <CRUD/>
+      {/* <BrowserRouter>
+      <Routes>
+     <Route path="products-filters" element={<ProductFilter/>}/>
+     </Routes>
+      </BrowserRouter> */}
+
+       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +26,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>  */}
     </div>
   );
 }
--- src/components/CRUD.js
+++ src/components/CRUD.js
@@ -16,29 +16,77 @@ function CRUD() {
 
     ]
     const [lists, setList] = useState(list)
+    const [updateState, setUpdateState]=useState(-1)
   return (
     <>
     <div className='crud'>
         <div>
         <Addlist setList={setList}/>
+        <form onSubmit={handleSubmit}>
         <table>
             {
                 lists.map((current)=>(
+                    updateState === current.id? <EditList current={current} lists={lists} setList={setList} />:
                 <tr>
                   <td>{current.name} </td>
                   <td> {current.price}  </td>
                   <td>
-                    <button className='delete'>Delete</button>
-                    <button className='edit'>Edit</button>
+                    <button className='delete' type='button' onClick={()=> handleDelete(current.id)}  >Delete</button>
+                    <button className='edit' onClick={()=>handleEdit(current.id)} >Edit</button>
                   </td>
                 </tr>
                  ) )
             }
         </table>
+        </form>
         </div>
     </div>
     </>
   )
+  function handleEdit(id){
+    setUpdateState(id)
+}
+function handleDelete(id){
+    const newlist = lists.filter((li)=>li.id !== id )
+    setList(newlist)
+}
+function handleSubmit(event){
+    event.preventDefault()
+    const name=event.target.elements.name.value
+    const price=event.target.elements.price.value
+    const newlist= lists.map((li)=>(
+        li.id=== updateState ? {...li, name:name, price:price}  :li
+        
+    ))
+    setList(newlist) 
+    setUpdateState(-1)
+}
+}
+function EditList({current, lists, setList}){
+    function handInputname(event){
+        const name=event.target.name;
+        const value=event.target.value;
+       const newlist= lists.map((li)=>(
+         li.id=== current.id ? {...li, name:value}  :li  
+        ))
+     setList(newlist) 
+    }
+    function handInputprice(event){
+        const name=event.target.name;
+        const value=event.target.value;
+       const newlist= lists.map((li)=>(
+         li.id=== current.id ? {...li, price:value}  :li  
+        ))
+     setList(newlist) 
+    }
+    return(
+        <tr>
+            <td><input type="text" onChange={handInputname} name="name" value={current.name} /></td>
+            <td><input type="text" onChange={handInputprice} name="price" value={current.price} /></td>
+            <td><button type="submit">Update</button></td>
+
+        </tr>
+    )
 }
 
 function Addlist({setList}){
@@ -49,7 +97,7 @@ function Addlist({setList}){
         const name = event.target.elements.name.value;
         const price = event.target.elements.price.value;
         const newlist ={
-            id:3,
+            id: 3,
             name,
             price
         }
